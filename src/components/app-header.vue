654683c090616588f5f990fc14cb7db9198547ae
s.zolotnitskii <sierri21@yandex.ru>
2023-01-06 17:01:56 +0300

    init

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,7 +1,10 @@
 <template>
-<header>
-  <nav>
-    <ul>
+<header class="header container">
+  <div class="logo">
+    <img src="" alt="logo">
+  </div>
+  <nav class="navigation">
+    <ul class="flex">
       <li v-for="link in links" :key="link.id">
         {{ link.text }}
       </li>
@@ -17,12 +20,22 @@
 
 <script setup>
 const links = [
-  {text: 'About', id: 'About'},
-  {text: 'Services', id: 'Services'},
-  {text: 'Analysis', id: 'Analysis'},
+  { text: 'About', id: 'About' },
+  { text: 'Services', id: 'Services' },
+  { text: 'Analysis', id: 'Analysis' },
 ]
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 40px;
+}
+.navigation {
+  ul {
+    grid-gap: 32px;
+  }
+}
 
 </style>
