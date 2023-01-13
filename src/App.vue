9cf09c252bec90ef223890650e68bb8919767a8c
s.zolotnitskii <sierri21@yandex.ru>
2023-01-13 22:48:39 +0300

    video

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="main-wrapper">
-    <index-page />
+    <div
+      v-if="preload"
+      class="preload"
+    >
+      <video
+        src="/video/preloader.mp4"
+        autoplay
+        muted
+        :controls="false"
+        @ended="videoHandler"
+      />
+    </div>
+
+    <index-page v-else />
   </div>
 </template>
 
 <script setup>
 import IndexPage from '~/pages/index-page.vue'
+import { ref } from 'vue'
+const preload = ref(true)
+
+const videoHandler = () => {
+	preload.value = false
+}
 </script>
 
 <style scoped lang="scss">
@@ -13,7 +32,16 @@ import IndexPage from '~/pages/index-page.vue'
   max-width: 100vw;
   width: 100%;
   background: url('/images/background.svg') center -172px / 100% 2361px  repeat-x;
-
-
+}
+.preload {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: black;
+  video {
+    width: 800px;
+  }
 }
 </style>
